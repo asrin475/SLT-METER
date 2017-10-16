018f4a17f338e835cc69cbbb8e8f8bc735a5b0c6
Asrin475 <asrin475@outlook.com>
2017-10-16 18:35:46 +0300

    Update Profiles.java

--- Profiles.java
+++ Profiles.java
@@ -1,1 +1,19 @@
 
+class Profiles extends BaseActivityCompat inplements httpTaskCompletionCallbacks{
+
+  
+  @Override
+  public void onStatus(){
+    /*
+    * handle server status codes
+    */
+    
+  }
+  
+  @Override 
+  public void onResponse(){
+    /*
+    * handle server response and to parse values
+    */
+  }
+}
